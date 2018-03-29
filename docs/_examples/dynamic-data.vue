590c5be59012195fb8811cd6de04bf1e9b59dc00
qingwei.li <cinwell.li@gmail.com>
2018-03-29 18:37:18 +0800

    docs: fix demo

--- docs/_examples/dynamic-data.vue
+++ docs/_examples/dynamic-data.vue
@@ -10,7 +10,7 @@
 <script>
 export default {
   data: () => ({
-    values: []
+    values: [{ value: 100 }]
   }),
 
   methods: {
--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ module.exports = {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: pkg.description}
+      {hid: 'description', name: 'description', content: pkg.description},
+      {name: 'theme-color', content: '#0e99e5'}
     ]
   },
   loading: {
